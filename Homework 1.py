4cfdafaea631dfca25b8891b9a73cdd73ca16ff7
jessepbaker <35543372+jessepbaker@users.noreply.github.com>
2018-12-14 01:17:06 -0700

    Add files via upload

--- Homework 1.py
+++ Homework 1.py
@@ -1,33 +1,26 @@
-def is_nugget_number(candidate, packets):
-    s = packets['s']
-    m = packets['m']
-    l = packets['l']
-    for i in range(candidate//s + 1):
-        for j in range(candidate//m + 1):
-            for k in range(candidate//l + 1):
-                if(candidtate == i*s + j*m +k*l):
+def is_nugget_number(candidate, SIX = 6, NINE = 9, TWENTY = 20):
+    for i in range(candidate//SIX + 1):
+        for j in range(candidate//NINE + 1):
+            for k in range(candidate//TWENTY + 1):
+                if(candidate == i * SIX + j * NINE + k * TWENTY):
                     return True
     
     return False
 
-packets = {'s' : 6, 'm' : 9, 'l' : 20}
 
-
-# Is a given number a nugget number
-candidate = packets['s']
-
-# Keep track of how many candidates in a row are nugget number
+## main
+sizes = {'S' : 6, 'M' : 9, 'L': 20}
+SIX = 6
 count = 0
+largest = 0
+candidate = SIX
 
-# Loop until stopping rule met
-while count < packets['s']:
-    if is_nugget_number(candidate, packets):
+while count < SIX:
+    if(is_nugget_number(candidate)):
         count += 1
     else:
-        count = 0
         largest = candidate
-    
+        count = 0
     candidate += 1
-    
-print(f"The largest number of nuggets that I cannot get is: {largest}")
-
+        
+print("The largest size that you cannot get is: {0}".format(largest))
